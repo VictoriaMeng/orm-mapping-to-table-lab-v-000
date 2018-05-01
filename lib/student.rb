22242deb81032098506193a05b53a27e4f1704ad
Victoria Meng <nights1ststar@protonmail.com>
2018-05-01 23:51:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,9 +6,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def initialize(name, grade)
+  def initialize(name, grade, id: nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
